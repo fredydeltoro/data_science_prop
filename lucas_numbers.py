01a8fe8f56718e513e7a5e7e484bc0ceddbf1207
Freddy Monroy <fredydeltoro@gmail.com>
2023-11-30 00:52:03 -0600

    Get a specific number of Lucas, through their closed [formula](https://en.wikipedia.org/wiki/Lucas_number\#Relationship_to_Fibonacci_numbers)

--- lucas_numbers.py
+++ lucas_numbers.py
@@ -1,3 +1,6 @@
+import math
+
+
 # Get lucas secuence
 def lucas_numbers(num_of_elements):
   numbers = [2, 1]
@@ -9,3 +12,16 @@ def lucas_numbers(num_of_elements):
 
 
 print(lucas_numbers(3))
+
+
+# Get a specific number of Lucas, through their closed formula https://en.wikipedia.org/wiki/Lucas_number#Relationship_to_Fibonacci_numbers
+def get_lucas_number(n):
+  phi = math.sqrt(5)
+
+  a = pow((1 + phi)/2, n)
+  b = pow((1 - phi)/2, n)
+
+  return a + b
+
+
+print('{:.0f}'.format(get_lucas_number(2)))
