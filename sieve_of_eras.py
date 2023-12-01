5bfb6256036818cea7a2bdd12030a7c0d04a8bd5
Freddy Monroy <fredydeltoro@gmail.com>
2023-12-01 17:50:24 -0600

    Get list of primes throught sieve of eratostenes

---
+++ sieve_of_eras.py
@@ -0,0 +1,24 @@
+def get_list_of_primes(n):
+  numbers = range(2, n+1)
+  primes = [2]
+  numbers = filter_mulitples(primes[0], numbers)
+
+  while True:
+    if pow(numbers[0], 2) < n:
+      primes.append(numbers[0])
+      numbers = filter_mulitples(numbers[0], numbers)
+    else:
+      break
+
+  return primes + numbers
+
+
+def filter_mulitples(number, arr):
+  for i, el in enumerate(arr):
+    if el % number == 0:
+      arr.pop(i)
+
+  return arr
+
+
+print(get_list_of_primes(100))
